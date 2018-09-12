fd99e9003ef9478e32dd0f79be32ef3529cea00d
Matthijs de Vos <m.devos96@gmail.com>
2018-09-12 14:20:47 +0200

    added seeders

--- database/seeds/ExerciseSeeder.php
+++ database/seeds/ExerciseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\course\Course;
+use App\Models\course\Exercise;
 use App\Models\course\Level;
 use Illuminate\Database\Seeder;
 
@@ -13,57 +14,14 @@ class ExerciseSeeder extends Seeder
    */
   public function run()
   {
-    $html = Course::where('name', 'HTML')->first();
-    $css = Course::where('name', 'CSS')->first();
-    $js = Course::where('name', 'JS')->first();
-
-    $beginner = Level::where('name', 'Beginner')->first();
-    $expert = Level::where('name', 'Expert')->first();
-
-    if (Exercise::where('title', 'Exercise1-html')->count != 0):
+    for($i=0;$i < 10; $i++):
       $exercise = new Exercise;
-      $exercise->title = 'Exercise1-html';
+      $exercise->course_id = Course::inRandomOrder()->first()->id;
       $exercise->content = Faker\Provider\Lorem::text(500);
-      $exercise->course_id = $html->id;
-      $exercise->level_id = $beginner->id;
-    endif;
-    if (Exercise::where('title', 'Exercise11-html')->count != 0):
-      $exercise = new Exercise;
-      $exercise->title = 'Exercise11-html';
-      $exercise->content = Faker\Provider\Lorem::text(600);
-      $exercise->course_id = $html->id;
-      $exercise->level_id = $expert->id;
-    endif;
-
-    if (Exercise::where('title', 'Exercise1-css')->count != 0):
-      $exercise = new Exercise;
-      $exercise->title = 'Exercise1-css';
-      $exercise->content = Faker\Provider\Lorem::text(500);
-      $exercise->course_id = $css->id;
-      $exercise->level_id = $beginner->id;
-    endif;
-    if (Exercise::where('title', 'Exercise11-css')->count != 0):
-      $exercise = new Exercise;
-      $exercise->title = 'Exercise11-css';
-      $exercise->content = Faker\Provider\Lorem::text(600);
-      $exercise->course_id = $css->id;
-      $exercise->level_id = $expert->id;
-    endif;
-
-    if (Exercise::where('title', 'Exercise1-js')->count != 0):
-      $exercise = new Exercise;
-      $exercise->title = 'Exercise1-js';
-      $exercise->content = Faker\Provider\Lorem::text(500);
-      $exercise->course_id = $js->id;
-      $exercise->level_id = $beginner->id;
-    endif;
-    if (Exercise::where('title', 'Exercise11-js')->count != 0):
-      $exercise = new Exercise;
-      $exercise->title = 'Exercise11-js';
-      $exercise->content = Faker\Provider\Lorem::text(600);
-      $exercise->course_id = $js->id;
-      $exercise->level_id = $expert->id;
-    endif;
-    
+      $final = rand(0,1);
+      $exercise->is_final = $final;
+      $exercise->level_id = Level::inRandomOrder()->first()->id;
+      $exercise->save();
+    endfor;
   }
 }

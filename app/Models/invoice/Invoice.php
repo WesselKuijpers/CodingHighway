762b8e2b1688065f7050b65806154f373e540475
Matthijs de Vos <m.devos96@gmail.com>
2018-09-12 13:30:58 +0200

    added invoice lines models relations

--- app/Models/invoice/Invoice.php
+++ app/Models/invoice/Invoice.php
@@ -2,9 +2,18 @@
 
 namespace App\Models\invoice;
 
+use App\Models\general\Organisation;
 use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    //
+  public function organisation()
+  {
+    return $this->belongsTo(Organisation::class);
+  }
+
+  public function InvoiceLines()
+  {
+    return $this->hasMany(InvoiceLine::class);
+  }
 }
--- app/Models/invoice/InvoiceLine.php
+++ app/Models/invoice/InvoiceLine.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class InvoiceLine extends Model
 {
-    //
+  public function invoice()
+  {
+    return $this->belongsTo(Invoice::class);
+  }
+
+  public function product()
+  {
+    return $this->belongsTo(Product::class);
+  }
 }

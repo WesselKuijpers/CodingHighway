a8afc3775160dcc0c7e1afddfa31153b7e2fb4b3
Matthijs de Vos <m.devos96@gmail.com>
2018-09-20 12:07:07 +0200

    added envoyer deploy method

---
+++ Envoy.blade.php
@@ -0,0 +1,70 @@
+@servers(['web' => 'ADSDCH@adsd.clow.nl'])
+
+@setup
+$git['url']     = 'git@gitlab.com:codinghighway/codinghighway.git';
+$git['branch']  = 'dev';
+
+$base           = '/home/adsdch/codinghighway';
+$shared         = ['vendor', 'node_modules', '.env'];
+$current        = \Carbon\Carbon::now()->format('YmdHis');
+@endsetup
+
+@story('deploy:cold')
+make:shared
+git:setup
+make:release
+make:link
+install
+set:live
+@endstory
+
+@story('deploy')
+git:update
+make:release
+make:link
+install
+set:live
+@endstory
+
+@task('install')
+cd {{ $base.'/releases/'.$current }}
+composer install
+npm install
+@endtask
+
+@task('git:setup')
+cd {{ $base }}
+cd {{ $base.'/repo' }}
+git clone {{ $git['url'] }} .
+git pull origin {{ $git['branch'] }}
+@endtask
+
+@task('git:update')
+git checkout {{ $git['branch'] }}
+git pull origin {{ $git['branch'] }}
+@endtask
+
+@task('make:shared')
+cd {{ $base }}
+mkdir shared repo releases shared/node_modules shared/vendor
+@endtask
+
+@task('make:release')
+cd {{ $base.'/releases' }}
+mkdir {{ $current }}
+cp -r {{ $base.'/repo/*' }} {{ $base.'/releases/'.$current.'/' }}
+@endtask
+
+@task('make:link')
+cd {{ $base.'/releases/'.$current }}
+@foreach($shared as $sh)
+  rm -rf {{ $sh }}
+  ln -s {{ $base.'/shared/'.$sh }} {{ $sh }}
+@endforeach
+@endtask
+
+@task('set:live')
+  cd {{ $base }}
+  rm -f current
+  ln -s {{ $base.'/releases/'.$current }} current
+@endtask

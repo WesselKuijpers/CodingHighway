9eaace1e9f6ff09ebfe9eff29215b81e903482ad
Matthijs de Vos <m.devos96@gmail.com>
2018-10-02 14:42:59 +0200

    whitespace

--- app/Http/Requests/ProgressRequest.php
+++ app/Http/Requests/ProgressRequest.php
@@ -3,28 +3,36 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class ProgressRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    if (Auth::user()):
+      return true;
+    else:
+      return false;
+    endif;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'user_id' => 'required|numeric',
+      'course_id' => 'required|numeric',
+      'lesson_id' => 'nullable|numeric',
+      'exercise_id' => 'nullable|numeric'
+    ];
+  }
 }

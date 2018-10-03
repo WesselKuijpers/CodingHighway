273ebe1c7be34ba9acbb95e788f95351a3907928
Thomas Spoelstra <s1090973@student.windesheim.nl>
2018-10-03 12:48:27 +0200

    major style bug fix at medium screen width

--- Modules/Course/Resources/views/course/index.blade.php
+++ Modules/Course/Resources/views/course/index.blade.php
@@ -11,26 +11,25 @@
         <div class="col-10 offset-1 mb-3">
             <hr>
         </div>
-
-    <div class="row">
-        {{-- Course loop --}}
-        @for($i = 0; $i < 9; $i++)
-            {{-- Course items --}}
-            <div class="col-lg-4 col-md-3 col-sm-1">
-                <a href="/course/$course->id">
-                    <div class="m-2" style="color:{{"#000"}}" class="no-link">
+        <div class="row">
+            {{-- Course loop --}}
+            @for($i = 0; $i < 9; $i++)
+                {{-- Course items --}}
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <a href="/course/$course->id" class="no-link">
+                        <div class="m-2" style="color:{{"#000"}}">
                             <div class="card h-100 overflow-h custom-card" style="background-color: {{"#333"}}; color:{{"#fff"}};">
-                                <div class="card-body text-center">
+                                <div class="card-body text-center text-white">
                                     <h5 class="card-title">$course->name</h5>
                                     <img class="card-img m-a h-200px w-auto" src="https://ubisafe.org/images/transparent-language-ruby.png" alt="Course logo">
                                     <p>$course->description Een omschrijving die wat informatie over de cursus geeft.</p>
                                 </div>
                             </div>
-                    </div>
-                </a>
-            </div>
-        @endfor
-    </div>
+                        </div>
+                    </a>
+                </div>
+            @endfor
+        </div>
 
 @endsection
 

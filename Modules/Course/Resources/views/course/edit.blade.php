1a1fe548fc01d3d3e69e4dce47b5769e39b8cdbf
Matthijs de Vos <m.devos96@gmail.com>
2018-10-04 13:52:38 +0200

    added errors to course edit | errors partial created

--- Modules/Course/Resources/views/course/edit.blade.php
+++ Modules/Course/Resources/views/course/edit.blade.php
@@ -6,21 +6,23 @@
 {{-- Placeholder for the page-specific content --}}
 @section('content')
 
-    {{-- Including the form title --}}
-    @include('shared.form_title', ['title' => "Pas een cursus aan"])
+  {{-- Including the form title --}}
+  @include('shared.form_title', ['title' => "Pas een cursus aan"])
+  @include('shared.error')
+  <form method="post" action="/course/{{$course['name']}}">
+    {{ method_field('PUT') }}
+    @include('shared.form_required', ['label' => 'Titel', 'name'=> 'name', 'type'=> 'text',
+    'value' => $course['name'], 'class' => 'form-control'])
+    @include('shared.textarea', ['label' => 'Beschrijving', 'name'=> 'description', 'type'=> 'text',
+    'required' => true, 'rows' => 10, 'value' => $course['description'], 'class' => ''])
+    @include('shared.form_required', ['label' => 'Organisatiekleur', 'name'=> 'color', 'type'=> 'color',
+    'value' => $course['color']])
+    @include('shared.form', ['label' => 'Cursusafbeelding', 'name' => 'media[]', 'type' => 'file',
+    'value' => 'file', 'class' => ''])
+    {{ csrf_field() }}
+    @include('shared.submit_button')
 
-    <form method="post" action="/course/{{$course['name']}}">
-        {{ method_field('PUT') }}
-        @include('shared.form_required', ['label' => 'Titel', 'name'=> 'name', 'type'=> 'text',
-        'value' => $course['name'], 'class' => 'form-control'])
-        @include('shared.textarea', ['label' => 'Beschrijving', 'name'=> 'description', 'type'=> 'text',
-        'required' => true, 'rows' => 10, 'value' => $course['description'], 'class' => ''])
-        @include('shared.form_required', ['label' => 'Organisatiekleur', 'name'=> 'color', 'type'=> 'color',
-        'value' => $course['color']])
-        @include('shared.form', ['label' => 'Cursusafbeelding', 'name' => 'media[]', 'type' => 'file',
-        'value' => 'file', 'class' => ''])
-        {{ csrf_field() }}
-        @include('shared.submit_button')
-    </form>
+
+  </form>
 
 @endsection

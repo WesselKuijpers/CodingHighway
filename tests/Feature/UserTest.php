be98f8941728bdcbe2a6a6ad2b1c7fca043b9b82
Matthijs de Vos <m.devos96@gmail.com>
2018-10-16 14:53:47 +0200

    test for registering new user

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,19 +2,42 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(false);
-    }
+  use RefreshDatabase;
+  use WithoutMiddleware;
+
+  public function testUserCreate()
+  {
+    $post = [
+      'firstname' => 'Testman',
+      'insertion' => 'the',
+      'lastname' => 'Tester',
+      'email' => 'test@test.nl',
+      'password' => 'testpass',
+      'password_confirmation' => 'testpass'
+    ];
+
+    $this->post(route('register'), $post);
+
+    $user = User::latest('id')->first();
+
+    if (
+      $user->firstname == 'Testman' &&
+      $user->insertion == 'the' &&
+      $user->lastname == 'Tester' &&
+      $user->email == 'test@test.nl'
+    ):
+      return $this->assertTrue(true);
+    else:
+      return $this->assertTrue(false);
+    endif;
+  }
 }

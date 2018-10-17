2a87432d94c1709ffc767ad41808bd3d84a31ca8
Matthijs de Vos <m.devos96@gmail.com>
2018-10-17 10:20:19 +0200

    added user edit test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,39 +4,39 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        if (env('APP_ENV') == 'testing'):
-            Schema::disableForeignKeyConstraints();
-            $this->call(LevelSeeder::class);
-            $this->call(CourseSeeder::class);
-            $this->call(TestLessonSeeder::class);
-            $this->call(TestExerciseSeeder::class);
-            Schema::enableForeignKeyConstraints();
-        else:
-            $this->call(PermissionSeeder::class);
-            $this->call(RoleSeeder::class);
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    if (env('APP_ENV') == 'testing'):
+      Schema::disableForeignKeyConstraints();
+      $this->call(LevelSeeder::class);
+      $this->call(CourseSeeder::class);
+      $this->call(TestLessonSeeder::class);
+      $this->call(TestExerciseSeeder::class);
+      Schema::enableForeignKeyConstraints();
+    else:
+      $this->call(PermissionSeeder::class);
+      $this->call(RoleSeeder::class);
 
-            $this->call(SaPermissionSeeder::class);
-            $this->call(AdminPermissionSeeder::class);
-            $this->call(UserPermissionSeeder::class);
+      $this->call(SaPermissionSeeder::class);
+      $this->call(AdminPermissionSeeder::class);
+      $this->call(UserPermissionSeeder::class);
 
-            $this->call(LicensesSeeder::class);
-            $this->call(OrganisationsSeeder::class);
-            $this->call(UserTableSeeder::class);
-            $this->call(UserOrganisationLicensesSeeder::class);
+      $this->call(LicensesSeeder::class);
+      $this->call(OrganisationsSeeder::class);
+      $this->call(UserTableSeeder::class);
+      $this->call(UserOrganisationLicensesSeeder::class);
 
-            $this->call(LevelSeeder::class);
-            $this->call(CourseSeeder::class);
-            $this->call(LessonSeeder::class);
-            $this->call(ExerciseSeeder::class);
+      $this->call(LevelSeeder::class);
+      $this->call(CourseSeeder::class);
+      $this->call(LessonSeeder::class);
+      $this->call(ExerciseSeeder::class);
 
-            $this->call(ForumSeeder::class);
-        endif;
-    }
+      $this->call(ForumSeeder::class);
+    endif;
+  }
 }
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\User;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -13,6 +14,13 @@ class UserTest extends TestCase
 {
   use RefreshDatabase;
   use WithoutMiddleware;
+
+  public function setUp()
+  {
+    parent::setUp();
+    Artisan::call('migrate', ['--env' => 'testing']);
+    Artisan::call('db:seed', ['--env' => 'testing']);
+  }
 
   public function testUserCreate()
   {
@@ -38,6 +46,45 @@ class UserTest extends TestCase
       return $this->assertTrue(true);
     else:
       return $this->assertTrue(false);
+    endif;
+  }
+
+  public function testUserEdit()
+  {
+    $post = [
+      'firstname' => 'Testman',
+      'insertion' => 'the',
+      'lastname' => 'Tester',
+      'email' => 'test@test.nl',
+      'password' => 'testpass',
+      'password_confirmation' => 'testpass'
+    ];
+
+    $this->post(route('register'), $post);
+    $user = User::latest('id')->first();
+
+    $post2 = [
+      'user_id' => $user->id,
+      'firstname' => 'Bugger',
+      'insertion' => 'de',
+      'lastname' => 'Bugman',
+      'email' => 'this@bug.com'
+    ];
+
+    $this->actingAs($user)
+         ->post(route('UserEditSave'), $post2);
+
+    $user = User::latest('id')->first();
+
+    if (
+      $user->firstname == 'Bugger' &&
+      $user->insertion == 'de' &&
+      $user->lastname == 'Bugman' //&&
+      //$user->email == 'this@bug.com'
+    ):
+      $this->assertTrue(true);
+    else:
+      $this->assertTrue(false);
     endif;
   }
 }

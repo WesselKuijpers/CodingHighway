d68524f9959d2c0e56186076b6a6a572844cdfcb
Thomas Spoelstra <s1090973@student.windesheim.nl>
2018-10-23 12:27:03 +0200

    styling of the organisation dashboard + fa installment

--- Modules/Organisation/Resources/views/show.blade.php
+++ Modules/Organisation/Resources/views/show.blade.php
@@ -1,5 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>Organisation dashboard</p>
-@stop
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header text-center">
+                        Dashboard van de organisatie OrganisatieNaam
+                    </div>
+
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-6">
+                                <p class="text-center">Gebruikers</p>
+                                <ul class="list-group">
+                                    @for($i=0;$i<8;$i++)
+                                        <li class="list-group-item">Gebruiker {{$i+1}}</li>
+                                        <i class="fas fa-chevron-down"></i>
+                                    @endfor
+                                </ul>
+
+
+                            </div>
+                            <div class="col-6">
+                                <p class="text-center">Licenties</p>
+                                <form action="GET">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="license_count">
+                                        <div class="col-md-6 m-auto">
+                                            <input type="submit" value="Genereer" class="btn btn-primary form-control mt-2">
+                                        </div>
+                                    </div>
+                                    @for($i=0;$i<app('request')->input('license_count'); $i++)
+                                        <p>{{$i+1}}</p>
+                                    @endfor
+                                </form>
+                            </div>
+                        </div>
+                        <div class="row">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,111 +15,118 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                        <div class="row">
-                            <div class="col-md-5 col-sm-12">
-                                <p class="font-weight-bold mt-3">Algemene informatie</p>
-                                <div class="row">
-                                    <div class="col-3">
-                                        <p>Naam:</p>
-                                    </div>
-                                    <div class="col-7">
-                                        <p>{{ Auth::user()->getFullname()}}</p>
-                                    </div>
+                    <div class="row">
+                        <div class="col-md-5 col-sm-12">
+                            <p class="font-weight-bold mt-3">Algemene informatie</p>
+                            <div class="row">
+                                <div class="col-md-3 col-sm-6">
+                                    <p>Naam:</p>
                                 </div>
-                                <div class="row">
-                                    <div class="col-3">
-                                        <p>Email:</p>
-                                    </div>
-                                    <div class="col-7">
-                                        <p>{{ Auth::user()->email }}</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-3">
-                                        <p>Organisatie:</p>
-                                    </div>
-                                    <div class="col-7">
-                                        <p>{{-- Auth::user()->organisation --}}</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-3">
-                                        <p>Licentiecode:</p>
-                                    </div>
-                                    <div class="col-6">
-                                        <p>{{ Auth::user()->license->first()->key }}</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-3">
-                                        <p>Geldig tot:</p>
-                                    </div>
-                                    <div class="col-6">
-                                        <p>{{ \Carbon\Carbon::parse(Auth::user()->license->first()->expires_at, 'UTC')->
-                                        format('d F Y')}}</p>
-                                    </div>
-                                    <div class="col-4">
-                                        <a href="{{url('user/edit')}}">
-                                            <input type="submit"  class="btn btn-primary" value="Gegevens aanpassen">
-                                        </a>
-                                    </div>
+                                <div class="col-md-7 col-sm-6">
+                                    <p>{{ Auth::user()->getFullname()}}</p>
                                 </div>
                             </div>
-                            <div class="col-md-7 col-sm-12">
-                                <p class="font-weight-bold mt-3">Voortgang van de lessen</p>
-                                <div class="row">
-                                    <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12 project wow animated animated3 fadeInLeft custom-card mb-3">
-                                        <div class="project-hover">
-                                            <h2>HTML</h2>
-                                            <hr />
-                                            <p>Les 2 - Maak een Html tag</p>
-                                            <div class="progress mb-2">
-                                                <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
-                                            </div>
-                                            <a href="#">Ga verder</a>
-                                        </div>
-                                    </div>
-                                    <div class="offset-md-1 offset-lg-1 offset-xl-1 col-xl-5 col-lg-5 col-md-5 col-sm-12 project wow animated animated3 fadeInLeft custom-card mb-3">
-                                        <div class="project-hover">
-                                            <h2>HTML</h2>
-                                            <hr />
-                                            <p>Les 2 - Maak een Html tag</p>
-                                            <div class="progress mb-2">
-                                                <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
-                                            </div>
-                                            <a href="#">Ga verder</a>
-                                        </div>
-                                    </div>
+                            <div class="row">
+                                <div class="col-md-3 col-sm-6">
+                                    <p>Email:</p>
+                                </div>
+                                <div class="col-md-7 col-sm-6">
+                                    <p>{{ Auth::user()->email }}</p>
                                 </div>
                             </div>
-                            <div class="col-md-7 offset-xl-5 offset-md-5 offset-lg-5 col-sm-12 offset-sm-0">
-                                <p class="font-weight-bold mt-3">Voortgang van de opdrachten</p>
-                                <div class="row">
-                                    <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12 project wow animated animated3 fadeInLeft custom-card mb-3">
-                                        <div class="project-hover">
-                                            <h2>HTML</h2>
-                                            <hr />
-                                            <p>Opdracht 8 - Maak een Html tag</p>
-                                            <div class="progress mb-2">
-                                                <div class="progress-bar" role="progressbar" style="width: 40%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">8 / 20</div>
-                                            </div>
-                                            <a href="#">Ga verder</a>
+                            <div class="row">
+                                <div class="col-md-3 col-sm-6">
+                                    <p>Organisatie:</p>
+                                </div>
+                                <div class="col-md-7 col-sm-6">
+                                    <p>{{-- Auth::user()->organisation --}}Windesheim Flevoland</p>
+                                </div>
+                            </div>
+                            <p class="font-weight-bold mt-3">Licentie</p>
+                            <div class="row">
+                                <div class="col-md-3 col-sm-6">
+                                    <p>Actief:</p>
+                                </div>
+                                <div class="col-md-7 col-sm-6">
+                                    @if (Auth::user()->license->first()->expired != 1)
+                                        <p>Ja</p>
+                                        @else
+                                        <p>Nee</p>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3 col-sm-6">
+                                    <p>Geldig tot:</p>
+                                </div>
+                                <div class="col-md-7 col-sm-6">
+                                    <p>{{ \Carbon\Carbon::parse(Auth::user()->license->first()->expires_at, 'UTC')->
+                                    format('d F Y')}}</p>
+                                </div>
+                                <div class="col-md-4 col-sm-6 offset-sm-30">
+                                    <a href="{{url('user/edit')}}">
+                                        <input type="submit"  class="btn btn-primary" value="Gegevens aanpassen">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-7 col-sm-12 d-table-cell">
+                            <p class="font-weight-bold mt-3">Voortgang van de lessen</p>
+                            <div class="row">
+                                <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12 project wow animated animated3 fadeInLeft custom-card mb-3">
+                                    <div class="project-hover">
+                                        <h2>HTML</h2>
+                                        <hr />
+                                        <p>Les 2 - Maak een Html tag</p>
+                                        <div class="progress mb-2">
+                                            <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
                                         </div>
+                                        <a href="#">Ga verder</a>
                                     </div>
-                                    <div class="offset-md-1 offset-lg-1 offset-xl-1 col-xl-5 col-lg-5 col-md-5 col-sm-12 project wow animated animated3 fadeInLeft custom-card mb-3">
-                                        <div class="project-hover">
-                                            <h2>CSS</h2>
-                                            <hr />
-                                            <p>Opdracht 2 - Maak een Html tag</p>
-                                            <div class="progress mb-2">
-                                                <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
-                                            </div>
-                                            <a href="#">Ga verder</a>
+                                </div>
+                                <div class="offset-md-1 offset-lg-1 offset-xl-1 col-xl-5 col-lg-5 col-md-5 col-sm-12 project wow animated animated3 fadeInLeft custom-card mb-3">
+                                    <div class="project-hover">
+                                        <h2>HTML</h2>
+                                        <hr />
+                                        <p>Les 2 - Maak een Html tag</p>
+                                        <div class="progress mb-2">
+                                            <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
                                         </div>
+                                        <a href="#">Ga verder</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-7 offset-xl-5 offset-md-5 offset-lg-5 col-sm-12 offset-sm-0">
+                            <p class="font-weight-bold mt-3">Voortgang van de opdrachten</p>
+                            <div class="row">
+                                <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12 project wow animated animated3 fadeInLeft custom-card mb-3">
+                                    <div class="project-hover">
+                                        <h2>HTML</h2>
+                                        <hr />
+                                        <p>Opdracht 8 - Maak een Html tag</p>
+                                        <div class="progress mb-2">
+                                            <div class="progress-bar" role="progressbar" style="width: 40%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">8 / 20</div>
+                                        </div>
+                                        <a href="#">Ga verder</a>
+                                    </div>
+                                </div>
+                                <div class="offset-md-1 offset-lg-1 offset-xl-1 col-xl-5 col-lg-5 col-md-5 col-sm-12 project wow animated animated3 fadeInLeft custom-card mb-3">
+                                    <div class="project-hover">
+                                        <h2>CSS</h2>
+                                        <hr />
+                                        <p>Opdracht 2 - Maak een Html tag</p>
+                                        <div class="progress mb-2">
+                                            <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
+                                        </div>
+                                        <a href="#">Ga verder</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

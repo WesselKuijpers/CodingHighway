43ba382aaaf5f5ebecaa3aa5904255c4bc0d8e6a
Matthijs de Vos <m.devos96@gmail.com>
2018-10-25 10:56:59 +0200

    Organsation activate

--- Modules/Organisation/Http/Controllers/OrganisationController.php
+++ Modules/Organisation/Http/Controllers/OrganisationController.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Organisation\Http\Controllers;
 
+use App\Models\general\Organisation;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
@@ -10,74 +11,100 @@ use OrganisationHelper;
 
 class OrganisationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Response
-     */
-    public function index()
-    {
-        return view('organisation::index');
-    }
+  public function __construct()
+  {
+    $this->middleware('LicenseCheck');
+    $this->middleware('role:sa')->only(['index']);
+    $this->middleware('permission:organisation.show')->only(['show']);
+    $this->middleware('permission:organisation.create')->only(['create', 'store']);
+    $this->middleware('permission:organisation.edit')->only(['edit', 'update']);
+    $this->middleware('permission:organisation.delete')->only(['destroy']);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function create()
-    {
-        return view('organisation::create');
-    }
+  /**
+   * Display a listing of the resource.
+   * @return Response
+   */
+  public function index()
+  {
+    $organisations = Organisation::all();
+    return view('organisation::index', compact('organisations'));
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function store(OrganisationRequest $request)
-    {
-        // $data variable attempts to create a organisation via the organisation helper, if successful return true, else return false.
-        $data = OrganisationHelper::create($request);
+  public function activate(OrganisationRequest $request)
+  {
+    $data = $request->validated();
 
-        // if $data is true redirect to the organisation overview, else redirect back with an error.
-        if ($data != false):
-            return redirect()->route('organisation');
-        else:
-            return back()->with('status', 'Er is iets mis gegaan met het verzenden!');
-        endif;
-    }
+    if (!empty($data['organisation_id'])):
+      $organisation = Organisation::find($data['organisation_id']);
+      $organisation->active = 1;
+      if ($organisation->save()):
+        return redirect()->route('organisation');
+      else:
+        return redirect()->back();
+      endif;
+    endif;
+  }
 
-    /**
-     * Show the specified resource.
-     * @return Response
-     */
-    public function show()
-    {
-        return view('organisation::show');
-    }
+  /**
+   * Show the form for creating a new resource.
+   * @return Response
+   */
+  public function create()
+  {
+    return view('organisation::create');
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     * @return Response
-     */
-    public function edit()
-    {
-        return view('organisation::edit');
-    }
+  /**
+   * Store a newly created resource in storage.
+   * @param  OrganisationRequest $request
+   * @return Response
+   */
+  public function store(OrganisationRequest $request)
+  {
+    // $data variable attempts to create a organisation via the organisation helper, if successful return true, else return false.
+    $data = OrganisationHelper::create($request);
 
-    /**
-     * Update the specified resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function update(Request $request)
-    {
-    }
+    // if $data is true redirect to the organisation overview, else redirect back with an error.
+    if ($data != false):
+      return redirect()->route('organisation');
+    else:
+      return back()->with('status', 'Er is iets mis gegaan met het verzenden!');
+    endif;
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     * @return Response
-     */
-    public function destroy()
-    {
-    }
+  /**
+   * Show the specified resource.
+   * @return Response
+   */
+  public function show()
+  {
+    return view('organisation::show');
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   * @return Response
+   */
+  public function edit()
+  {
+    return view('organisation::edit');
+  }
+
+  /**
+   * Update the specified resource in storage.
+   * @param  Request $request
+   * @return Response
+   */
+  public function update(Request $request)
+  {
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   * @return Response
+   */
+  public function destroy()
+  {
+  }
 }
--- Modules/Organisation/Http/routes.php
+++ Modules/Organisation/Http/routes.php
@@ -1,16 +1,18 @@
 <?php
 
 Route::group(['middleware' => 'web', 'prefix' => 'organisation', 'namespace' => 'Modules\Organisation\Http\Controllers'], function () {
-    Route::resource('/', 'OrganisationController', [
-        'names' => [
-                'index' => 'organisation',
-                'store' => 'organisation.store',
-                'create' => 'organisation.create',
-                'show' => 'organisation.show',
-                'destroy' => 'organisation.destroy',
-                'update' => 'organisation.update',
-                'edit' => 'organisation.edit',
-            ],
-        ]
-    )->parameter('', 'id');
+  Route::resource('/', 'OrganisationController', [
+      'names' => [
+        'index' => 'organisation',
+        'store' => 'organisation.store',
+        'create' => 'organisation.create',
+        'show' => 'organisation.show',
+        'destroy' => 'organisation.destroy',
+        'update' => 'organisation.update',
+        'edit' => 'organisation.edit',
+      ],
+    ]
+  )->parameter('', 'id');
+
+  Route::post('/activate', 'OrganisationController@activate')->name('organisation.activate');
 });

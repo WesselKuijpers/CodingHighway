d94088daf2d12e151bbf86916ac5e3b7f7ba1a92
Matthijs de Vos <m.devos96@gmail.com>
2018-10-30 14:15:28 +0100

    enabled base css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,12 +20,9 @@
   @if (Auth::user())
     @if(Auth::user()->organisation())
       <link href="{{ asset('css/organisations/organisation'.Auth::user()->organisation()->id.'.css') }}" rel="stylesheet">
-    @else
-      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @endif
-  @else
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   @endif
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.datatables.net/rowreorder/1.2.5/css/rowReorder.dataTables.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">

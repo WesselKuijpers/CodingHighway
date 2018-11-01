7fd1d52a33b19744a86bbf2ab527fb242b8c6b82
Thomas Spoelstra <s1090973@student.windesheim.nl>
2018-11-01 10:57:47 +0100

    Tweaked the register page and created a custom button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,34 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">{{ __('Register') }}</div>
+    <div class="row">
+        <div class="col-12">
+            <div class="container contact-form">
+                    @include('shared.form_title', ['title' => "Registreer"])
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
 
-          <div class="card-body">
-            <form method="POST" action="{{ route('register') }}">
-              @csrf
-
-              @include('shared.form_required', ['label' => 'Voornaam', 'name'=> 'firstname', 'type'=> 'text'])
-              @include('shared.form',          ['label' => 'Tussenvoegsel', 'name'=> 'insertion', 'type'=> 'text'])
-              @include('shared.form_required', ['label' => 'Achternaam', 'name'=> 'lastname', 'type'=> 'text'])
-              @include('shared.form_required', ['label' => 'Email', 'name'=> 'email', 'type'=> 'email'])
-              @include('shared.form_required', ['label' => 'Wachtwoord', 'name'=> 'password', 'type'=> 'password'])
-              @include('shared.form_required', ['label' => 'Wachtwoord bevestigen', 'name'=> 'password_confirmation', 'type'=> 'password'])
-
-              <div class="form-group row mb-0">
-                <div class="col-md-6 offset-md-4">
-                  <button type="submit" class="btn btn-primary">
-                    {{ __('Register') }}
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
+                    @include('shared.form_required', ['label' => 'Voornaam', 'name'=> 'firstname', 'type'=> 'text', 'class' => 'form-control'])
+                    @include('shared.form',          ['label' => 'Tussenvoegsel', 'name'=> 'insertion', 'type'=> 'text', 'class' => 'form-control'])
+                    @include('shared.form_required', ['label' => 'Achternaam', 'name'=> 'lastname', 'type'=> 'text', 'class' => 'form-control'])
+                    @include('shared.form_required', ['label' => 'Email', 'name'=> 'email', 'type'=> 'email', 'class' => 'form-control'])
+                    @include('shared.form_required', ['label' => 'Wachtwoord', 'name'=> 'password', 'type'=> 'password', 'class' => 'form-control'])
+                    @include('shared.form_required', ['label' => 'Wachtwoord bevestigen', 'name'=> 'password_confirmation', 'type'=> 'password', 'class' => 'form-control'])
+                    @include('shared.submit_button')
+                    @include('shared.error')
+                </form>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 @endsection

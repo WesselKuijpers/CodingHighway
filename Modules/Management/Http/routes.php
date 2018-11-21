29d55678b8b282722b79606174e30a33a57d8fed
Matthijs de Vos <m.devos96@gmail.com>
2018-11-21 11:36:00 +0100

    removed old controller

--- Modules/Management/Http/routes.php
+++ Modules/Management/Http/routes.php
@@ -1,18 +1,7 @@
 <?php
 
 Route::group(['middleware' => 'web', 'prefix' => 'management', 'namespace' => 'Modules\Management\Http\Controllers'], function () {
-  Route::resource('/admin', 'AdminController', [
-    'names' => [
-      'index' => 'admin',
-      'store' => 'admin.store',
-      'create' => 'admin.create',
-      'destroy' => 'admin.destroy',
-      'update' => 'admin.update',
-      'edit' => 'admin.edit'
-    ]
-  ])->only(['index', 'store'])->parameter('', 'id');
-
-  Route::resource('/role', 'RoleController', [
+    Route::resource('/role', 'RoleController', [
     'names' => [
       'index' => 'role',
       'store' => 'role.store',

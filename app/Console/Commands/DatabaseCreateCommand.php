81a0ecd18bff2670a8f00191c090570d2089f908
Matthijs de vos <m.devos96@gmail.com>
2018-12-12 09:46:20 +0100

    using env vars in command

--- app/Console/Commands/DatabaseCreateCommand.php
+++ app/Console/Commands/DatabaseCreateCommand.php
@@ -35,17 +35,20 @@ class DatabaseCreateCommand extends Command
   public function handle()
   {
     $database = env('DB_DATABASE', false);
+    $database_course = env('DB_DATABASE_COURSE', false);
+    $database_general = env('DB_DATABASE_GENERAL', false);
+    $database_forum = env('DB_DATABASE_FORUM', false);
 
-    if (!$database) {
+    if (!$database || !$database_course || !$database_forum || !$database_general) {
       $this->info('Skipping creation of database as env(DB_DATABASE) is empty');
       return;
     }
 
     $list = [
       $database,
-      "{$database}_course",
-      "{$database}_forum",
-      "{$database}_general",
+      $database_course,
+      $database_forum,
+      $database_general,
     ];
 
     try {
